d57063f3441856bef6b0ffb62b8120950d54ec9b
Badri Vishal Singhal <badrivishalsinghal8803@gmail.com>
2023-04-03 15:16:11 +0530

    Added CORS to resolve cross oringin error

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const express = require('express');
 const predictionRouter = require('./Routers/prediction');
 const authRouter = require('./Routers/authRouter');
 const houseRouter = require('./Routers/houseRouter');
+const cors = require('cors')
 require('./Models/DBConnection')
 
 const app = express();
+app.use(cors());
 app.listen(process.env.PORT || 3000);
 app.use(express.json())
 app.use(cookieParser());
